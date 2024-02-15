8f74071f882dce0f970efb07b6860aff3ec0e3eb
terry-u16 <terry-u16@users.noreply.github.com>
2024-02-16 03:16:05 +0900

    Revert "正規分布の累積分布関数の計算方法変更"
    This reverts commit 2d236454c4ad8b3e0dbeaf9ea67c9f564fbd02dd.

--- src/distributions.rs
+++ src/distributions.rs
@@ -10,35 +10,9 @@ impl GaussianDistribution {
 
     /// 累積分布関数F(x)を計算する
     pub fn calc_cumulative_dist(&self, x: f64) -> f64 {
-        // 正規分布の確率密度関数と累積分布関数の計算
-        // https://marui.hatenablog.com/entry/2023/01/23/194507
+        // ロジスティック近似する
         let x = (x - self.mean) / self.std_dev;
-
-        let result = if x == 0.0 {
-            0.5
-        } else if x > 0.0 {
-            Self::calc_cumulative_dist_inner(x)
-        } else {
-            1.0 - Self::calc_cumulative_dist_inner(-x)
-        };
-
-        result
-    }
-
-    fn calc_cumulative_dist_inner(x: f64) -> f64 {
-        const P: f64 = 0.2316419;
-        let t1 = 1.0 / (1.0 + P * x);
-        let t2 = t1 * t1;
-        let t3 = t2 * t1;
-        let t4 = t3 * t1;
-        let t5 = t4 * t1;
-        1.0 - Self::dnorm(x)
-            * (0.319381530 * t1 - 0.356563782 * t2 + 1.781477937 * t3 - 1.821255978 * t4
-                + 1.330274429 * t5)
-    }
-
-    fn dnorm(x: f64) -> f64 {
-        (-0.5 * x * x).exp() / (2.0 * std::f64::consts::PI).sqrt()
+        1.0 / (1.0 + (-0.07056 * x * x * x - 1.5976 * x).exp())
     }
 }
 
