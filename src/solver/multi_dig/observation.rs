f6cb046f8eafe863a1578c0ca51052e1cc4713e6
terry-u16 <terry-u16@users.noreply.github.com>
2024-02-19 14:38:52 +0900

    Revert "1マスのみ掘るやつを追加"
    This reverts commit 61c6f9dc85dcb8f4c3ffbe4773dfab4a8ec10ec3.

--- src/solver/multi_dig/observation.rs
+++ src/solver/multi_dig/observation.rs
@@ -175,13 +175,8 @@ impl Observation {
 
             for true_v in 0..likelihoods_len {
                 let v = true_v as f64;
-                let (mean, variance) = if pos.len() == 1 {
-                    (v as f64, 1e-5)
-                } else {
-                    let mean = (k - v) * input.eps + v * (1.0 - input.eps);
-                    let variance = k * input.eps * (1.0 - input.eps);
-                    (mean, variance)
-                };
+                let mean = (k - v) * input.eps + v * (1.0 - input.eps);
+                let variance = k * input.eps * (1.0 - input.eps);
                 let std_dev = variance.sqrt();
                 let gauss = GaussianDistribution::new(mean, std_dev);
 
